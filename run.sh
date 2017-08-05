78e9822673d04244f0e3aedcd2bdef5e5ebbbfa9
Michael Schwartz <mykesx@gmail.com>
2017-08-05 16:53:21 -0700

    working denon microservice

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+docker run \
+    -d \
+    --rm \
+    --name="denon-microservice" \
+    denon-microservice
---
+++ shell.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker run -it --rm --name denon-microservice denon-microservice /bin/bash
